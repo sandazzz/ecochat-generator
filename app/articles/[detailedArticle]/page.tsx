e1b770b294bbce7505f86a28068e7603dbfab24e
sandazzz <rsandarisoa@gmail.com>
2024-04-17 19:10:15 +0200

    Update page.tsx

--- app/articles/[detailedArticle]/page.tsx
+++ app/articles/[detailedArticle]/page.tsx
@@ -11,42 +11,44 @@ export default async function detailedArticles({
   const article = await getArticle(id);
 
   return (
-    <main className="h-full relative">
-      <div className="absolute top-36 left-0 right-0 px-6 md:px-52 text-white">
-        <div
-          id="main-content"
-          className="flex flex-col md:flex-row w-full justify-between"
-        >
-          <div>
-            <h1>{article.title}</h1>
-            <p>Auteur: {article.author}</p>
-            <p>
-              Date de publication: {new Date(article.publishedAt).toLocaleDateString()}
-            </p>
-            <p>Description: {article.description}</p>
-          </div>
+    <main className="h-full relative top-28 px-6 md:px-72 text-white">
+      <div id="basic-information" className="flex flex-col md:flex-row w-full justify-between gap-5 mb-8">
+        <div>
+          <h1 className="text-3xl mb-1">{article.title}</h1>
+          <p>Auteur: {article.author}</p>
+          <p>
+            Date de publication:{" "}
+            {new Date(article.publishedAt).toLocaleDateString()}
+          </p>
+          <p>Description: {article.description}</p>
+        </div>
 
+        <div className="w-80 md:w-96 h-52 relative">
           <Image
-            width={384}
-            height={200}
-            objectFit="cover"
+            className="rounded-md"
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            sizes="100vw "
+            priority={true}
             src={article.illustration}
             alt="Illustration"
           />
         </div>
+      </div>
 
-        <div id="content" className="flex flex-col gap-6">
-          {article.sections.map((section: any) => (
-            <div key={section.order} className="flex flex-col gap-3">
-              <h2>{section.title} :</h2>
-              <ul>
-                {section.paragraphes.map((paragraphe: any) => (
-                  <li key={paragraphe.id}>{paragraphe.content}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
+      <div id="content" className="flex flex-col gap-6">
+        {article.sections.map((section: any) => (
+          <div key={section.order} className="flex flex-col gap-5">
+            <h2 className="text-2xl">{section.title} :</h2>
+            <ul className="flex flex-col gap-2">
+              {section.paragraphes.map((paragraphe: any) => (
+                <li key={paragraphe.id}>{paragraphe.content}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </main>
   );
