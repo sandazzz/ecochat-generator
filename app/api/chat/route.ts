430e8c9ff5f9685c61b7d0ed873cdae407085a60
sandazzz <rsandarisoa@gmail.com>
2024-04-19 16:38:13 +0200

    Chat update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,32 +1,38 @@
+// import the OpenAI API client (that's edge friendly!)
 import { openai } from "@/src/lib/openai";
-import { NextResponse } from "next/server";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
-export const POST = async (req: Request) => {
-  const { prompt } = await req.json();
+//Create a systemPrompt for prompt engenering
+/*const systemPrompt = `
+Context :
+You are MyAi an ai text generetor that answer to question about ecology.
+You are an expert in the domain of ecology and environement. 
 
+Goal : 
+Answer with the best precision with some example based on the given prompt.
+
+Criteria : 
+-Anwser in english if the question is in english
+-Anwser in french if the question is in french
+-if it is not a question about ecology respond that you can't anwser
+
+`*/
+
+// Set the runtime to edge for best performance
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: "gpt-4-0125-preview",
-    messages: [
-      {
-        role: "assistant",
-        content: `Be an expert in ecology`,
-      },
-      {
-        role: "user",
-        content: prompt,
-      },
-    ],
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages,
   });
-  return NextResponse.json({
-    chat : response.choices[0].message.content,
-  });
-};
 
-/*import { NextResponse } from "next/server";
-
-export const POST = async (req: Request) => {
-  return NextResponse.json({
-    ok: true,
-  });
-};
-*/
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,49 +1,27 @@
 "use client";
 
-import { useState } from "react";
-import { CornerDownLeft } from "lucide-react";
+import { useChat } from "ai/react";
 
 export default function Chat() {
-  const [chat, setChat] = useState("");
-  const handlesubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault;
-
-    const formData = new FormData(event.currentTarget);
-
-    const prompt = formData.get("prompt") as string;
-
-    const result = await fetch("api/chat", {
-      method: "POST",
-      body: JSON.stringify({ prompt }),
-    });
-
-    const json = await result.json();
-
-    setChat(json.chat)
-  };
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
     <main className="h-full relative">
-      <div className="absolute top-20 left-0 right-0 flex items-center justify-center">
-        <progress className=" progress progress-accent w-56" />
-      </div>
-      
-      <h1 className="text-lg">{chat}</h1>
+      <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+        {messages.map((m) => (
+          <div key={m.id} className=" p-4 text-white whitespace-pre-wrap">
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
+          </div>
+        ))}
 
-      <div className="fixed bottom-4 left-0 right-0 flex items-center justify-center">
-        <form
-          className="p-4 bg-base-200 max-w-lg w-full"
-          onSubmit={handlesubmit}
-        >
-          <fieldset className="flex gap-4 items-start">
-            <textarea
-              name="prompt"
-              className="w-full textarea textarea-primary"
-            />
-            <button className="btn btn-outline" type="submit">
-              <CornerDownLeft size={16} />
-            </button>
-          </fieldset>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl input input-bordered input-info"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
         </form>
       </div>
     </main>
